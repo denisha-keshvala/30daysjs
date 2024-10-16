6e4587f94fb528c73a142f9b17528e0092729c9e
denisha_keshvala <denishakeshvala@gmail.com>
2024-10-16 17:59:15 +0530

    added 2 solution of day 1

---
+++ day1/ex-1/index.js
@@ -0,0 +1,4 @@
+// 1. Write a single line comment which says, comments can make code readable
+       // var string = "denisha";
+       
+// 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
