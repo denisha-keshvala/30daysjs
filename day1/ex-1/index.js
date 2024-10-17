3cb6ce825c9da0e39d9bf5bd42419e1bed856a0d
denisha_keshvala <denishakeshvala@gmail.com>
2024-10-17 11:52:31 +0530

    added 13 file into the 30daysofjs

--- day1/ex-1/index.js
+++ day1/ex-1/index.js
@@ -1,4 +1,73 @@
 // 1. Write a single line comment which says, comments can make code readable
        // var string = "denisha";
        
+
 // 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+      // WELCOME TO 30DaysOfJavascript
+
+
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+      /* comments can make code readable, easy to reuse and informative*/
+
+    
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data
+       let string= "denisha keshvala";
+       console.log(string);
+       let thank;
+       console.log(thank);
+       let temp = null;
+
+
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+       console.log(typeof "denisha");
+       console.log(typeof  123);
+       console.log(typeof undefined);
+       console.log(typeof null);
+       console.log(typeof false);
+       console.log(typeof 7899);
+
+
+// 6. Declare four variables without assigning values
+        let variable1;
+        let variable2;
+        let variable3;
+        let variable4;
+
+
+// 7. Declare four variables with assigned values
+       let strings= "denisha";
+       let rollno= 1;
+       let name=true;
+       let task= undefined;
+       console.log(strings);
+       console.log(rollno);
+       console.log(name);
+       console.log(task);
+
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+       let firstname= 'denisha',lastname= 'keshvala',meritalstatus= 'single',country= 'india',age= 21
+       console.log(firstname, lastname, meritalstatus, country, age);
+
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+       // let firstname= 'denisha';
+       // let lastname= 'keshvala';
+       // let maritalstatus= 'single';
+       // let country= 'india';
+       // let age= 21;
+       // console.log(firstname);
+       // console.log(lastname);
+       // console.log(maritalstatus);
+       // console.log(country);
+       // console.log(age);
+
+       
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+//like
+//I am 25 years old.
+//You are 30 years old.
+       let myage=25;
+       let yourAge=30;
+       console.log("i am "+ myage + "years old");
+       console.log("you are" + yourAge + "years old");
